a24eab77c1fae575e3ca71d49aafcc3c426005db
Glitch (hello-webpage) <none>
2018-03-14 21:57:55 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-# My cool website
+CommunitySupport.show
+=================
 
-This file describes your project to the community. What's your cool website about? What makes it special?
+A list of community support items for professionals in live entertainment and adjacent industries.
 
-ヽ(๏∀๏ )ﾉ
+Made by [Glitch](https://glitch.com/)
+-------------------
 
-## ← index.html
-
-Where you'll write the content of your website. 
-
-## ← styles.css
-
-CSS files add styling rules to your content
-
-## ← script.js
-
-If you're feeling fancy you can add interactivity to your site with Javascript
-
+\ ゜o゜)ノ
--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log('hi');
+"use strict";
+
+Vue.component('item', {
+  props: ['title'],
+  template: `
+    <li class="item-li">
+      <h3 class="item-heading">{{ title }}</h3>
+      <slot></slot>
+    </li>
+    `
+});
+
+Vue.component('page-section', {
+  props: ['slug', 'title'],
+  template: `
+  <section class="page-section">
+    <hr class="section-hr">
+    <h2 class="section-heading"><a :href="'#' + slug" class="section-heading-link">{{ title }}</a></h2>
+
+    <p class="description"><slot name="desc"></slot></p>
+
+    <ul>
+      <slot></slot>
+    </ul>
+  </section>
+  `
+});
+
+var vm = new Vue({
+  el: "#app"
+});
